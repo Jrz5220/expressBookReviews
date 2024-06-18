ec01d2363ad2b4e3124220414dae2204ba162ea6
Jrz5220 <felixlazo94@gmail.com>
2024-06-18 14:40:22 -0400

    Create get request for getting book reviews based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,8 +53,10 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // get book reviews based on ISBN
+  let isbn = req.params.isbn;
+  let bookReviews = books[isbn].reviews;
+  return res.status(200).json(bookReviews);
 });
 
 module.exports.general = public_users;
